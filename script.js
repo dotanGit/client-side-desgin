48f691e09bd744dc21bb1d42bdc628f1a6979868
dotanGit <frenkeldotan1998@gmail.com>
2024-07-14 09:12:54 +0300

    Added single page view details

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// Navbar icon for smaller page size
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -13,3 +16,27 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+
+
+
+// Single product details - changing products images
+
+var mainImg = document.getElementById('mainImg');
+var smallImg = document.getElementsByClassName('small-img')
+
+smallImg[0].onclick = function() {
+    mainImg.src = smallImg[0].src;
+}
+
+smallImg[1].onclick = function() {
+    mainImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = function() {
+    mainImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = function() {
+    mainImg.src = smallImg[3].src;
+}
+
